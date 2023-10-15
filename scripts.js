8f45c6a4bd935de565679db5c9e197c23d7c1a18
GeneralGLA-true <artemstarodub@gmail.com>
2023-10-15 11:35:58 +0300

    Update scripts.js
    homework 4 (Математичні операції)

--- scripts.js
+++ scripts.js
@@ -6,20 +6,32 @@ alert('Hello, '+ user +'! How are you?'); */
 
 /////3.1
 
-const firstString = prompt('введіть першу строку');
+/* const firstString = prompt('введіть першу строку');
 const secondString = prompt('введіть другу строку');
 const thirdString = prompt('введіть третю строку');
 
 //alert(`${firstString} ${secondString} ${thirdString}`);
-alert(firstString + secondString + thirdString);
+alert(firstString + secondString + thirdString); */
 
 ////3.2
 
-const someNumbers = prompt("Введіть п'ятизначне число", 55555);
+/* const someNumbers = prompt("Введіть п'ятизначне число", 55555);
 let sepStr = new String;
 
 for (let i = 0; i < someNumbers.length; i++) {
   sepStr += someNumbers[i] + ' ';
 }
 
-alert(sepStr);
+alert(sepStr); */
+
+
+/////////////////////////////////////// homework 4
+
+const a = +prompt("Введіть число A", 5);
+const b = +prompt("Введіть число B", 5);
+
+alert(`
+  ${a} + ${b} = ${a + b}
+  ${a} - ${b} = ${a - b}
+  ${a} * ${b} = ${a * b}
+  ${a} / ${b} = ${a / b}`);
