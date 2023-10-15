e51371d843d81d6d92e11c9a2a1e60c89a2badfe
GeneralGLA-true <artemstarodub@gmail.com>
2023-10-15 11:18:39 +0300

    Update scripts.js
    homework 3

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
-const user = prompt("Вкажіть своє ім'я");
+/* const user = prompt("Вкажіть своє ім'я");
 
-alert('Hello, '+ user +'! How are you?');
+alert('Hello, '+ user +'! How are you?'); */
+
+//////////// homework 3
+
+/////3.1
+
+const firstString = prompt('введіть першу строку');
+const secondString = prompt('введіть другу строку');
+const thirdString = prompt('введіть третю строку');
+
+//alert(`${firstString} ${secondString} ${thirdString}`);
+alert(firstString + secondString + thirdString);
+
+////3.2
+
+const someNumbers = prompt("Введіть п'ятизначне число", 55555);
+let sepStr = new String;
+
+for (let i = 0; i < someNumbers.length; i++) {
+  sepStr += someNumbers[i] + ' ';
+}
+
+alert(sepStr);
