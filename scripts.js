360aeea4ebfbb956ec9e6e2aa43ae2e699540913
GeneralGLA-true <artemstarodub@gmail.com>
2023-10-18 15:31:52 +0300

    Homework 6
    Портрет користувача

--- scripts.js
+++ scripts.js
@@ -42,3 +42,88 @@ alert(`
 /* const hours = prompt("Вкажіть кількість годин", 24);
 const seconds = hours * 3600;
 alert(`У ${hours} годинах ${seconds} секунд`); */
+
+
+//////////////////////////////////    homework 6
+
+const user = {};
+const capital = "Київ" || "Лондон" || "Вашингтон";
+const currentYear = 2023;
+let age = '';
+let locationUser = '';
+let sportLog = '';
+
+user.birthYear = prompt(`Вкажіть рік народження`, 1995);
+user.cityResidense = prompt(`Вкажіть місто проживання`, 'Харків');
+user.favoriteSport = prompt(`Вкажіть улюблений вид спорту`, 'Лакрос');
+
+switch (null) {
+  case user.birthYear:
+    alert('Шкода, що Ви не захотіли ввести свій рік народження');
+      
+  case user.cityResidense:
+    alert('Шкода, що Ви не захотіли ввести свіє місто');
+   
+  case user.favoriteSport:
+    alert('Шкода, що Ви не захотіли ввести свій улюблений вид спорту');
+      
+  default:
+    break;
+}
+
+if (user.birthYear) {
+  age = `Тобі ${currentYear - user.birthYear} рочків`;
+} else {
+  age = 'Розтин покаже твій вік';
+}
+
+/* console.log(typeof user.cityResidense)
+console.log(user.cityResidense === String)
+switch (user.cityResidense) {
+
+  case capital:
+    locationUser = `Ти живеш у столиці ${user.cityResidense}`;
+    break;
+
+  case ??????????:
+    locationUser = `Ти живеш у місті ${user.cityResidense}`;
+    break;
+
+  default:
+    locationUser = 'Тобі все одно не сховатись...';
+    break;
+} */
+
+
+if (user.cityResidense === capital) {
+  locationUser = `Ти живеш у столиці ${user.cityResidense}`;
+} else if (user.cityResidense === null){
+  locationUser = 'Тобі все одно не сховатись...';
+} else {
+  locationUser = `Ти живеш у місті ${user.cityResidense}`;
+}
+
+switch (user.favoriteSport) {
+  case 'Лакрос':
+  sportLog = 'Круто! Хочеш стати Полом Рабілем?';
+    break;
+
+  case 'Керлінг':
+  sportLog = 'Круто! Хочеш стати Джоном Шустером?';
+    break;
+  
+  case 'Текбол':
+    sportLog = 'Круто! Хочеш стати Адамом Блажовичем?'; 
+    break;
+  case null:
+    sportLog = 'Кімнатна людинка?';
+  default:
+    sportLog = 'Цікаво...'; 
+    break;
+}
+
+alert(`
+${age},
+${locationUser};
+${sportLog};
+`)
